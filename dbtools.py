58af9f4e8b63eb5a538d0be98c3f54c6246caae4
GUY <guy1998@gmail.com>
2025-01-26 19:02:39 +0200

    finish

--- dbtools.py
+++ dbtools.py
@@ -1,22 +1,5 @@
 import inspect
 
-#given function
-
-#def orm(cursor, dto_type):
- #   # the following line retrieve the argument names of the constructor
-  #  args = inspect.getargspec(dto_type.__init__).args
-#
- #   # the first argument of the constructor will be 'self', it does not correspond
-    # to any database field, so we can ignore it.
-  #  args = args[1:]
-
-    # gets the names of the columns returned in the cursor
-   # col_names = [column[0] for column in cursor.description]
-
-    # map them into the position of the corresponding constructor argument
-    #col_mapping = [col_names.index(arg) for arg in args]
-    #return [row_map(row, col_mapping, dto_type) for row in cursor.fetchall()]
-
 #updated function
 def orm(cursor, dto_type):
     # Retrieve the argument names of the constructor using inspect.signature
